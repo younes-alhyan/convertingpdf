5984201c8503c1c704aae5f8e1db269284fdb506
Yokai-47 <gameryounes555@gmail.com>
2025-08-18 14:47:01 +0100

    Add '/api'Server Proxy

--- vite.config.ts
+++ vite.config.ts
@@ -8,12 +8,17 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    proxy: {
+      "/api": {
+        target: "https://convertingpdf.onrender.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
