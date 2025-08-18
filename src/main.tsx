80004d00be657180cdf24e23fc1994bb75b639e9
Yokai-47 <gameryounes555@gmail.com>
2025-08-18 18:53:06 +0100

    Add improvements comment

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,10 @@ import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
+/*To improve:
+    *./src/components/tools:
+        Reset Result state when selecting new files 
+        Drag & Drop for better UX
+        Check Response.json if valid Result object
+        Use Result.message for toasts
+*/
